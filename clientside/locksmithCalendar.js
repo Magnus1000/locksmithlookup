a00d888b0005d8ec772e2f4b80442f021e49f7b3
Jack McDonald <jackmagnusmcdonald@gmail.com>
2024-03-14 08:57:09 -0400

    Updates

--- clientside/locksmithCalendar.js
+++ clientside/locksmithCalendar.js
@@ -1,95 +1,67 @@
-//import React, { useState } from 'react';
-//import { Checkbox, FormControlLabel, Typography } from '@mui/material';
-const { Checkbox, FormControlLabel, Typography } = MaterialUI;
+const { Checkbox, FormControlLabel, Typography, Select, MenuItem } = MaterialUI;
 
 const AvailabilitySelector = () => {
   const [availability, setAvailability] = React.useState({
-    tuesday: false,
-    wednesday: false,
-    thursday: false,
-    friday: false,
-    saturday: false,
+    monday: { available: true, startTime: '9:00am', endTime: '5:00pm' },
+    tuesday: { available: true, startTime: '9:00am', endTime: '5:00pm' },
+    wednesday: { available: true, startTime: '9:00am', endTime: '5:00pm' },
+    thursday: { available: true, startTime: '9:00am', endTime: '5:00pm' },
+    friday: { available: true, startTime: '9:00am', endTime: '5:00pm' },
+    saturday: { available: true, startTime: '9:00am', endTime: '5:00pm' },
   });
 
-  const handleChange = (day) => {
-    setAvailability({ ...availability, [day]: !availability[day] });
+  const handleAvailabilityChange = (day) => {
+    setAvailability({
+      ...availability,
+      [day]: { ...availability[day], available: !availability[day].available },
+    });
   };
+
+  const handleTimeChange = (day, field, value) => {
+    setAvailability({
+      ...availability,
+      [day]: { ...availability[day], [field]: value },
+    });
+  };
+
+  const renderTimeDropdown = (day, field) => (
+    <Select
+      value={availability[day][field]}
+      onChange={(e) => handleTimeChange(day, field, e.target.value)}
+    >
+      <MenuItem value="8:30am">8:30am</MenuItem>
+      <MenuItem value="8:45am">8:45am</MenuItem>
+      <MenuItem value="9:00am">9:00am</MenuItem>
+      <MenuItem value="9:15am">9:15am</MenuItem>
+      <MenuItem value="9:30am">9:30am</MenuItem>
+      <MenuItem value="5:00pm">5:00pm</MenuItem>
+    </Select>
+  );
 
   return (
     <div>
       <Typography variant="h6">Weekly hours</Typography>
-      <FormControlLabel
-        control={
-          <Checkbox
-            checked={availability.tuesday}
-            onChange={() => handleChange('tuesday')}
-          />
-        }
-        label={
-          <>
-            <Typography>TUE</Typography>
-            <Typography variant="body2">9:00am - 5:00pm</Typography>
-          </>
-        }
-      />
-      <FormControlLabel
-        control={
-          <Checkbox
-            checked={availability.wednesday}
-            onChange={() => handleChange('wednesday')}
-          />
-        }
-        label={
-          <>
-            <Typography>WED</Typography>
-            <Typography variant="body2">9:00am - 5:00pm</Typography>
-          </>
-        }
-      />
-      <FormControlLabel
-        control={
-          <Checkbox
-            checked={availability.thursday}
-            onChange={() => handleChange('thursday')}
-          />
-        }
-        label={
-          <>
-            <Typography>THU</Typography>
-            <Typography variant="body2">9:00am - 5:00pm</Typography>
-          </>
-        }
-      />
-      <FormControlLabel
-        control={
-          <Checkbox
-            checked={availability.friday}
-            onChange={() => handleChange('friday')}
-          />
-        }
-        label={
-          <>
-            <Typography>FRI</Typography>
-            <Typography variant="body2">9:00am - 5:00pm</Typography>
-          </>
-        }
-      />
-      <FormControlLabel
-        control={
-          <Checkbox
-            checked={availability.saturday}
-            onChange={() => handleChange('saturday')}
-          />
-        }
-        label={
-          <>
-            <Typography>SAT</Typography>
-            <Typography variant="body2">9:00am - 5:00pm</Typography>
-          </>
-        }
-      />
+      {['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'].map((day) => (
+        <FormControlLabel
+          key={day}
+          control={
+            <Checkbox
+              checked={availability[day].available}
+              onChange={() => handleAvailabilityChange(day)}
+            />
+          }
+          label={
+            <>
+              <Typography>{day.toUpperCase()}</Typography>
+              {renderTimeDropdown(day, 'startTime')}
+              <Typography variant="body2">-</Typography>
+              {renderTimeDropdown(day, 'endTime')}
+            </>
+          }
+        />
+      ))}
     </div>
   );
 };
 
-ReactDOM.render(React.createElement(AvailabilitySelector), document.getElementById('root'));
+ReactDOM.render(<AvailabilitySelector />, document.getElementById('root'));
