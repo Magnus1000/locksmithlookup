df36ef70358e20c9bae3f5cdeb3cab519233dd0c
Jack McDonald <jackmagnusmcdonald@gmail.com>
2024-03-15 11:25:22 -0400

    Update

--- clientside/accountPage.js
+++ clientside/accountPage.js
@@ -300,7 +300,7 @@ const VerticalNav = ({ setActivePage }) => {
           </div>
         </div>
         <div className="vertical-nav-bottom">
-            <button className="vertical-nav-menu-item">
+            <button className="vertical-nav-menu-item" data-ms-action="logout">
                 Logout
             </button>
         </div>
