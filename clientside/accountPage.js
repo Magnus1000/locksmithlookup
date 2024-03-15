48b57e59d23e1eed492294668210d4543d693596
Jack McDonald <jackmagnusmcdonald@gmail.com>
2024-03-15 14:03:55 -0400

    Updates

--- clientside/accountPage.js
+++ clientside/accountPage.js
@@ -13,6 +13,7 @@ const AvailabilitySelector = () => {
 
     const [tempAvailability, setTempAvailability] = React.useState(availability);
     const [isEditing, setIsEditing] = React.useState(false);
+    const [isSaving, setIsSaving] = React.useState(false);
 
 
     React.useEffect(() => {
@@ -31,6 +32,8 @@ const AvailabilitySelector = () => {
     };
 
     const handleSave = () => {
+        setIsSaving(true);
+
         // Transform tempAvailability into an array of objects
         const availability = Object.entries(tempAvailability).map(([day_of_week, { startTime, endTime }]) => ({
             day_of_week,
@@ -54,9 +57,11 @@ const AvailabilitySelector = () => {
             .then((data) => {
                 setAvailability(tempAvailability);
                 setIsEditing(false);
+                setIsSaving(false);
             })
             .catch((error) => {
                 console.error('Error:', error);
+                setIsSaving(false);
             });
     };
 
@@ -141,6 +146,16 @@ const AvailabilitySelector = () => {
             ))}
         </Select>
     );
+
+    // Loading Icon
+    const LoadingIcon = () => {
+        return (
+            <svg className="loading-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                <path fill="currentColor" d="M256 44a20 20 0 1 1 0 40 20 20 0 1 1 0-40zM204 64a52 52 0 1 0 104 0A52 52 0 1 0 204 64zm28 384a24 24 0 1 0 48 0 24 24 0 1 0 -48 0zM472 256a24 24 0 1 0 -48 0 24 24 0 1 0 48 0zM76 256a12 12 0 1 1 -24 0 12 12 0 1 1 24 0zM64 212a44 44 0 1 0 0 88 44 44 0 1 0 0-88zm344.7-74.8a24 24 0 1 1 -33.9-33.9 24 24 0 1 1 33.9 33.9zm22.6-56.6a56 56 0 1 0 -79.2 79.2 56 56 0 1 0 79.2-79.2zM92.2 391.8a28 28 0 1 0 56 0 28 28 0 1 0 -56 0zM384 408a24 24 0 1 0 0-48 24 24 0 1 0 0 48zM131.5 131.5a16 16 0 1 1 -22.6-22.6 16 16 0 1 1 22.6 22.6zm22.6-45.3A48 48 0 1 0 86.3 154.2a48 48 0 1 0 67.9-67.9z"
+                />
+            </svg>
+        );
+    };
 
     return (
         <div className="availabilty-page-body">
@@ -341,7 +356,7 @@ const AvailabilitySelector = () => {
                             },
                         }}
                     >
-                        Save and Close
+                        {isSaving ? <LoadingIcon /> : 'Save and Close'}
                     </Button>
                 </div>
             ) : null}
